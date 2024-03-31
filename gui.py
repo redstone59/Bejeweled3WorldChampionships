6777620eefd5c5ed8c383ffd3f68d22195d3fee8
redstone59 <109836856+redstone59@users.noreply.github.com>
2024-03-31 18:00:10 +1000

    so close to done
    added displaying of subchallenge strings, scores, multipliers, multiplied scores, and final score

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, filedialog, messagebox
 from challenges import *
 
 import queue, hashlib
@@ -19,8 +19,8 @@ def sha256_sum(string_to_hash: str):
 
 class GraphicalUserInterface:
     def __init__(self):
-        self.gui_queue = queue.Queue()     # Receives commands from BJ3WC (new challenges, scores, etc)
-        self.action_queue = queue.Queue()  # Sends actions to BJ3WC (abort challenge, open new challenge, etc etc)
+        self.gui_queue = queue.Queue()           # Receives commands from BJ3WC (new challenges, scores, etc)
+        self.action_queue = queue.Queue()        # Sends actions to BJ3WC (abort challenge, open new challenge, etc etc)
         self.font = RenderFont(get_from_resources("Flare Gothic Regular.ttf"))
         self.challenge: Challenge | None = None
         
@@ -31,9 +31,84 @@ class GraphicalUserInterface:
         
         self.size = tk.StringVar(value = "high") # Initialise GUI at "high" resolution
         self.show_time_left = tk.BooleanVar(value = False)
+        self.index = 0
+        self.labels = {"scores": [],
+                       "challenges": [],
+                       "multipliers": [],
+                       "multiplied_scores": [],
+                       "metadata": []}
         
         self.set_up_menu_bar()
         self.set_resolution()
+    
+    def add_subchallenge_score(self, score: int):
+        text_position = {"normal": (38, 148 + 35 * self.index), # Estimated vertical offset
+                         "high": (49, 190 + 46 * self.index),
+                         "ultra": (76, 297 + 72 * self.index)
+                         }[self.size.get()]
+        size = {"normal": 18,
+                "high": 21,
+                "ultra": 30
+                }[self.size.get()]
+        
+        self.labels["scores"] += [ImageTk.PhotoImage(self.font.get_render(size, f"{score:,}", colour = "#f4f4d0", align = "ls"))]
+        self.canvas.create_image(text_position, image = self.labels["scores"][-1])
+        
+        self.index += 1
+    
+    def add_subchallenge_text(self, text: str):
+        text_position = {"normal": (38, 130 + 35 * self.index), # Estimated vertical offset
+                         "high": (49, 167 + 46 * self.index),
+                         "ultra": (76, 261 + 72 * self.index)
+                         }[self.size.get()]
+        size = {"normal": 18,
+                "high": 21,
+                "ultra": 30
+                }[self.size.get()]
+        
+        self.labels["challenges"] += [ImageTk.PhotoImage(self.font.get_render(size, text, colour = "#f4f4d0", align = "ls"))]
+        self.canvas.create_image(text_position, image = self.labels["challenges"][-1])
+    
+    def add_multiplied_scores(self, scores: dict):
+        size = {"normal": 18,
+                "high": 21,
+                "ultra": 30
+                }[self.size.get()]
+        
+        self.index = 0
+        final_score = 0
+        
+        for key in scores:
+            multiplier_position = {"normal": (262, 130 + 35 * self.index), # Estimated vertical offset
+                                "high": (336, 167 + 46 * self.index),
+                                "ultra": (526, 261 + 72 * self.index)
+                                }[self.size.get()]
+            score_position = {"normal": (262, 148 + 35 * self.index), # Estimated vertical offset
+                            "high": (336, 190 + 46 * self.index),
+                            "ultra": (526, 297 + 72 * self.index)
+                            }[self.size.get()]
+            score = scores[key]
+            
+            self.labels["multipliers"] += [ImageTk.PhotoImage(self.font.get_render(size, f"x{score["multiplier"]:,}", colour = "#ffff60", align = "rs"))]
+            self.canvas.create_image(multiplier_position, image = self.labels["multipliers"][-1])
+            
+            self.labels["multiplied_scores"] += [ImageTk.PhotoImage(self.font.get_render(size, f"{score["score"] * score["multiplier"]:,}", colour = "#ffff60", align = "rs"))]
+            self.canvas.create_image(score_position, image = self.labels["multiplied_scores"][-1])
+            
+            final_score += score["score"] * score["multiplier"]
+            self.index += 1
+        
+        final_score_position = {"normal": (150, 522),
+                                "high": (196, 709),
+                                "ultra": (300, 1110),
+                                }[self.size.get()]
+        size = {"normal": 30,
+                "high": 42,
+                "ultra": 54
+                }[self.size.get()]
+        
+        self.labels["multiplied_scores"] += [ImageTk.PhotoImage(RenderFont(get_from_resources("Myriad Pro Regular.OTF")).get_render(size, f"{final_score:,}"))]
+        self.canvas.create_image(final_score_position, image = self.labels["multiplied_scores"][-1])
     
     def add_metadata_text(self):
         if self.challenge == None: return
@@ -44,33 +119,33 @@ class GraphicalUserInterface:
                 }[self.size.get()]
         index = ["normal", "high", "ultra"].index(self.size.get())
         
-        self.name_text = ImageTk.PhotoImage(self.font.get_render(size, self.challenge.name))
+        self.labels["metadata"] += [ImageTk.PhotoImage(self.font.get_render(size, self.challenge.name))]
         self.canvas.create_image([(179, 35),
                                   (250, 44),
                                   (399, 69)
                                   ][index],
-                                 image = self.name_text)
-        self.author_text = ImageTk.PhotoImage(self.font.get_render(size, self.challenge.author))
+                                 image = self.labels["metadata"][-1])
+        self.labels["metadata"] += [ImageTk.PhotoImage(self.font.get_render(size, self.challenge.author))]
         self.canvas.create_image([(179, 51),
                                   (250, 67),
                                   (399, 104)
                                   ][index],
-                                 image = self.author_text)
+                                 image = self.labels["metadata"][-1])
         description = insert_newlines(self.challenge.description, 30)[:60] # Split every 30 characters, and trim after 60 characters
-        self.description_text = ImageTk.PhotoImage(self.font.get_render(size, description, align = "la"))
+        self.labels["metadata"] += [ImageTk.PhotoImage(self.font.get_render(size, description, align = "la"))]
         self.canvas.create_image([(121, 66),
                                   (155, 85),
                                   (243, 133)
                                   ][index],
-                                 image = self.description_text)
+                                 image = self.labels["metadata"][-1])
         
         hash_text = f"Challenge hash:\n{sha256_sum(self.challenge.to_dict())}"
-        self.challenge_hash_text = ImageTk.PhotoImage(self.font.get_render([7, 9, 12][index], hash_text, align = "mm"))
+        self.labels["metadata"] += [ImageTk.PhotoImage(self.font.get_render([7, 9, 12][index], hash_text, align = "mm"))]
         self.canvas.create_image([(150, 470),
                                   (196, 601),
                                   (300, 940)
                                   ][index],
-                                 image = self.challenge_hash_text)
+                                 image = self.labels["metadata"][-1])
     
     def close(self):
         # I know I could put the below into one if but that would be a long ass line of code.
@@ -79,7 +154,12 @@ class GraphicalUserInterface:
                 return
         
         self.root.destroy()
-        exit()
+        self.gui_queue.put("abort")
+#       exit()
+    
+    def open_challenge_file(self):
+        file = filedialog.askopenfilename(parent = self.root, title = "Open challenge file", filetypes = [("JSON file", ".json")])
+        return file
     
     def set_resolution(self):
         resolution = {"normal": "300x620",
@@ -129,18 +209,17 @@ class GraphicalUserInterface:
                               )
         
         submenu_resolution = tk.Menu(menu_file, tearoff = False)
-        submenu_resolution.add_radiobutton(label='Normal (300x600)',
+        submenu_resolution.add_radiobutton(label = 'Normal (300x600)',
                                            value = "normal",
                                            variable = self.size,
                                            command = self.set_resolution,
-                                           state = 'disabled'
                                            )
-        submenu_resolution.add_radiobutton(label='High (384x768)',
+        submenu_resolution.add_radiobutton(label = 'High (384x768)',
                                            value = "high",
                                            variable = self.size,
                                            command = self.set_resolution
                                            )
-        submenu_resolution.add_radiobutton(label='Ultra (600x1200)',
+        submenu_resolution.add_radiobutton(label = 'Ultra (600x1200)',
                                            value = "ultra",
                                            variable = self.size,
                                            command = self.set_resolution
@@ -162,14 +241,16 @@ class GraphicalUserInterface:
         
         menu_challenge.add_command(label = "Abort",
                                    underline = 0,
-                                   state = "disabled")
+                                   state = "disabled"
+                                   )
         
         menu_challenge.add_checkbutton(label = "Show time left",
                                        underline = 0,
                                        state = 'active',
                                        variable = self.show_time_left,
                                        onvalue = True,
-                                       offvalue = False)
+                                       offvalue = False
+                                       )
         
         challenge_button["menu"] = menu_challenge
         challenge_button.place(x = 60, y = 0)
@@ -178,28 +259,24 @@ class GraphicalUserInterface:
         
         menu_about = tk.Menu(about_button, tearoff = False)
         menu_about.add_command(label = "Challenge",
-                               underline = 0)
+                               underline = 0
+                               )
         menu_about.add_command(label = "Documentation",
                                underline = 0,
                                command = lambda: web_open('about:blank'), # need to rewrite that
-                               state = "disabled")
+                               state = "disabled"
+                               )
         menu_about.add_command(label = "GitHub page",
                                underline = 0,
-                               command = lambda: web_open("https://github.com/redstone59/Bejeweled3WorldChampionships"))
+                               command = lambda: web_open("https://github.com/redstone59/Bejeweled3WorldChampionships")
+                               )
         menu_about.add_command(label = "Find a bug?",
                                underline = 7,
-                               command = lambda: web_open("https://github.com/redstone59/Bejeweled3WorldChampionships/issues"))
+                               command = lambda: web_open("https://github.com/redstone59/Bejeweled3WorldChampionships/issues")
+                               )
         
         about_button["menu"] = menu_about
         about_button.place(x = 150, y = 0)
     
     def start(self):
         self.root.mainloop()
-
-g = GraphicalUserInterface()
-
-c = Challenge("impossible challenge xviii", "Matthew :D", "impossible challenge, number 1 most impossible...", "timed", [], 40)
-g.challenge = c
-g.add_metadata_text()
-
-g.start()
